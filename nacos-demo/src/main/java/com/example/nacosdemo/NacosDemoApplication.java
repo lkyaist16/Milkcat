1a317ea69c72cb23dd5d2a06d1ddf7e4e5930402
lkyi <lkyi@senthink.com>
2019-06-21 10:01:04 +0800

    Initial commit

---
+++ nacos-demo/src/main/java/com/example/nacosdemo/NacosDemoApplication.java
@@ -0,0 +1,29 @@
+package com.example.nacosdemo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosDemoApplication.class, args);
+    }
+
+
+    @Slf4j
+    @RestController
+    static class TestController {
+        @GetMapping("/hello")
+        public String hello(@RequestParam String name) {
+            log.info("invoked name = " + name);
+            return "hello " + name;
+        }
+    }
+}
