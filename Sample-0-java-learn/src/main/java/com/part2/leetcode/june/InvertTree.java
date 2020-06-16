d0aff2b5d67dcbf78366fb8d479076563b95093c
lkyi <lkyi@senthink.com>
2020-06-16 13:42:31 +0800

    feat(leetcode): 添加反转二叉树相关

---
+++ Sample-0-java-learn/src/main/java/com/part2/leetcode/june/InvertTree.java
@@ -0,0 +1,64 @@
+package com.part2.leetcode.june;
+
+
+/**
+ * 翻转一棵二叉树。
+ * <p>
+ * 示例：
+ * <p>
+ * 输入：
+ * <p>
+ * 4
+ * /   \
+ * 2     7
+ * / \   / \
+ * 1   3 6   9
+ * <p>
+ * 输出：
+ * <p>
+ * 4
+ * /   \
+ * 7     2
+ * / \   / \
+ * 9   6 3   1
+ */
+public class InvertTree {
+
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return root;
+        }
+        if (root.left == null && root.right == null) {
+            return root;
+        }
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        if (root.left != null) {
+            invertTree(root.left);
+        }
+        if (root.right != null) {
+            invertTree(root.right);
+        }
+        return root;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public static void main(String[] args) {
+
+
+
+
+    }
+}
+
+
