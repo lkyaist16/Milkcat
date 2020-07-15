261f768b42639523936e3847004bb3877af4725e
lkyi <lkyi@senthink.com>
2020-07-15 14:07:49 +0800

    fix:力扣打卡

---
+++ Sample-0-java-learn/src/main/java/com/part2/leetcode/july/NumTrees.java
@@ -0,0 +1,45 @@
+package com.part2.leetcode.july;
+
+/**
+ * 给定一个整数 n，求以 1 ... n 为节点组成的二叉搜索树有多少种？
+ *
+ * 示例:
+ *
+ * 输入: 3
+ * 输出: 5
+ * 解释:
+ * 给定 n = 3, 一共有 5 种不同结构的二叉搜索树:
+ *
+ *    1         3     3      2      1
+ *     \       /     /      / \      \
+ *      3     2     1      1   3      2
+ *     /     /       \                 \
+ *    2     1         2                 3
+ *
+ * 动态规划：
+ *
+ * G(n)=f(1)+f(2)+f(3)+f(4)+...+f(n)
+ *
+ * 当i为根节点时，其左子树节点个数为i-1个，右子树节点为n-i，则
+ * f(i) = G(i-1)*G(n-i)
+ *
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/unique-binary-search-trees
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class NumTrees {
+    public int numTrees(int n) {
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i < n + 1; i++) {
+            for (int j = 1; j < i + 1; j++) {
+                dp[i] += dp[j - 1] * dp[i - j];
+            }
+        }
+
+        return dp[n];
+    }
+}
