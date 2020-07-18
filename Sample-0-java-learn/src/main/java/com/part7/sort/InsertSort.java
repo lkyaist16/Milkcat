90f27a8193061889ef0710b6d40e1e72a3cfd5cb
lkyi <269577519@qq.com>
2020-07-18 11:22:17 +0800

    提交

--- Sample-0-java-learn/src/main/java/com/part7/sort/InsertSort.java
+++ Sample-0-java-learn/src/main/java/com/part7/sort/InsertSort.java
@@ -4,19 +4,31 @@ import java.util.Arrays;
 
 /**
  * 插入排序
- *
- *
+ * <p>
+ * 从第二位开始比较
+ * <p>
+ * 默认第一位是排好序的
+ * <p>
+ * 然后插入到相应的位置
  */
 public class InsertSort {
 
     public static Integer[] insertSort(Integer[] numbers) {
-        for (int i = 0; i < numbers.length; i++) {
-            for (int j = i+1; j > 0; j--) {
+        Integer temp;
 
+        for (int i = 0; i < numbers.length - 1; i++) {
+            for (int j = i + 1; j > 0; j--) {
+                if (numbers[j] < numbers[j - 1]) {
+                    temp = numbers[j - 1];
+                    numbers[j - 1] = numbers[j];
+                    numbers[j] = temp;
+                } else {
+                    break;
+                }
             }
         }
 
-      return numbers;
+        return numbers;
     }
 
     public static void main(String[] args) {
