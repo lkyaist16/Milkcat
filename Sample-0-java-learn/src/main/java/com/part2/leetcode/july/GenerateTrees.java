a321017b4daeea09ad296e2d281f95498bfe431a
lkyi <269577519@qq.com>
2020-07-21 23:52:08 +0800

    提交

--- Sample-0-java-learn/src/main/java/com/part2/leetcode/july/GenerateTrees.java
+++ Sample-0-java-learn/src/main/java/com/part2/leetcode/july/GenerateTrees.java
@@ -1,5 +1,8 @@
 package com.part2.leetcode.july;
 
+import sun.plugin.PluginURLJarFileCallBack;
+
+import java.util.LinkedList;
 import java.util.List;
 
 
@@ -39,11 +42,38 @@ import java.util.List;
  */
 public class GenerateTrees {
     public List<TreeNode> generateTrees(int n) {
+        if (n == 0) {
+            return new LinkedList<TreeNode>();
+        }
+        return generateTrees(1, n);
+    }
 
+    public List<TreeNode> generateTrees(int start, int end) {
+        List<TreeNode> allTrees = new LinkedList<TreeNode>();
+        if (start > end) {
+            allTrees.add(null);
+            return allTrees;
+        }
 
+        // 枚举可行根节点
+        for (int i = start; i <= end; i++) {
+            // 获得所有可行的左子树集合
+            List<TreeNode> leftTrees = generateTrees(start, i - 1);
 
-        return null;
+            // 获得所有可行的右子树集合
+            List<TreeNode> rightTrees = generateTrees(i + 1, end);
 
+            // 从左子树集合中选出一棵左子树，从右子树集合中选出一棵右子树，拼接到根节点上
+            for (TreeNode left : leftTrees) {
+                for (TreeNode right : rightTrees) {
+                    TreeNode currTree = new TreeNode(i);
+                    currTree.left = left;
+                    currTree.right = right;
+                    allTrees.add(currTree);
+                }
+            }
+        }
+        return allTrees;
     }
 
     public class TreeNode {
