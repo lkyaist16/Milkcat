4f1deba2a54f1b7a84b5b9b2fc6b60ce5e59089b
lkyi <lkyi@senthink.com>
2020-07-23 20:53:47 +0800

    fix:力扣打卡

--- Sample-0-java-learn/src/main/java/com/part2/leetcode/july/MinPathSum.java
+++ Sample-0-java-learn/src/main/java/com/part2/leetcode/july/MinPathSum.java
@@ -22,23 +22,24 @@ package com.part2.leetcode.july;
  */
 public class MinPathSum {
     public static int minPathSum(int[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+
         int m = grid.length;
         int n = grid[0].length;
 
-        if(m == 0 || n == 0) {
-            return 0;
-        }
 
         int[][] dp = new int[m][n];
 
         dp[0][0] = grid[0][0];
 
         for (int i = 1; i < m; i++) {
-            dp[0][i] = dp[0][i -1] + grid[0][i];
+            dp[i][0] = dp[i - 1][0] + grid[i][0];
         }
 
         for (int i = 1; i < n; i++) {
-            dp[i][0] = dp[i - 1][0] + grid[i][0];
+            dp[0][i] = dp[0][i -1] + grid[0][i];
         }
 
         for (int i = 1; i < m; i++) {
