5e3c640850ba2ca154b912bf0b4d5fc5ce9107e9
lkyi <lkyi@senthink.com>
2020-11-02 10:32:20 +0800

    feat:力扣打卡

---
+++ Sample-0-java-learn/src/main/java/com/part2/leetcode/october/IslandPerimeter.java
@@ -0,0 +1,46 @@
+package com.part2.leetcode.october;
+
+/**
+ *
+ */
+public class IslandPerimeter {
+    public int islandPerimeter(int[][] grid) {
+        int res = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == 1) {
+                    if (j - 1 >= 0) {
+                        if (grid[i][j - 1] == 0) {
+                            res++;
+                        }
+                    }else  {
+                        res++;
+                    }
+                    if (i - 1 >= 0) {
+                        if (grid[i - 1][j] == 0) {
+                            res++;
+                        }
+                    }else {
+                        res++;
+                    }
+                    if (j + 1 < grid[i].length) {
+                        if (grid[i][j + 1] == 0) {
+                            res++;
+                        }
+                    } else {
+                        res ++;
+                    }
+                    if (i + 1 < grid.length) {
+                        if (grid[i + 1][j] == 0) {
+                            res++;
+                        }
+                    } else {
+                        res++;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+
+}
